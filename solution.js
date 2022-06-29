3813b726376aa81b1c30e875694734e0c5c2911f
Kairos22 <alandiko84@gmail.com>
2022-06-29 17:01:01 +0200

    add exercise2

--- solution.js
+++ solution.js
@@ -1,1 +1,37 @@
+// 1
+const str = 'exercise JavaScript';
+console.log(str)
+
+// 2
+console.log(str[1] , str[5])
+
+// 3
+firstName = 'Alan'
+
+// 4
+age = 38
+
+// 5
+year = 2022
+
+// 6
+console.log(firstName + ' is ' + age + ' in ' +  year  )
+
+// 7
+const isMarried = true;
+
+// 8
+console.log(firstName + " is married: " , isMarried)
+
+// 9
+const programmingLanguage = 'JavaScript';
+const isFun = true;
+console.log(programmingLanguage + 'is fun:' + isFun)
+ 
+// 10
+console.log(programmingLanguage[0], programmingLanguage[4])
+
+// 11
+console.log(programmingLanguage.length);
+
 
